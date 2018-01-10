b1365663f2d7e3d88fc30924d99471ae57d06f86
leeey <heyleezy@gmail.com>
2018-01-10 21:26:06 +0800

    Initial Commit

---
+++ stock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stock
+//
+//  Created by zhiyuan10 on 2018/1/10.
+//  Copyright © 2018年 钱宝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
